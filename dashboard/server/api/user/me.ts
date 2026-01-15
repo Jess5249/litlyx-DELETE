77a578ab61cd167be71fcc6f653371160d185d0e
OWNER <183957814+Jess5249@users.noreply.github.com>
2026-01-14 23:07:24 -0600

    Delete dashboard directory
    ABUSE - UNAUTHORIZED

--- dashboard/server/api/user/me.ts
+++
@@ -1,6 +0,0 @@
-import { AuthContext } from "~/server/middleware/01-authorization";
-
-export default defineEventHandler(async event => {
-    const userData: AuthContext = getRequestUser(event) as any;
-    return userData;
-});
